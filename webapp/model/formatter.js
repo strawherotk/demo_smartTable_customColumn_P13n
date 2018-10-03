0e19e4a07c5e7743ba958ebf86d24b827138b66e
S0016142257 <vuong.hoquoc@vn.bosch.com>
2018-10-03 05:41:08 +0000

    [smart table] Add Product table - with custom column using P13nData.

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -1,23 +1,33 @@
-sap.ui.define([
-	], function () {
-		"use strict";
+sap.ui.define([], function () {
+	"use strict";
 
-		return {
-			/**
-			 * Rounds the currency value to 2 digits
-			 *
-			 * @public
-			 * @param {string} sValue value to be formatted
-			 * @returns {string} formatted currency value with 2 digits
-			 */
-			currencyValue : function (sValue) {
-				if (!sValue) {
-					return "";
-				}
-
-				return parseFloat(sValue).toFixed(2);
+	return {
+		/**
+		 * Rounds the currency value to 2 digits
+		 *
+		 * @public
+		 * @param {string} sValue value to be formatted
+		 * @returns {string} formatted currency value with 2 digits
+		 */
+		currencyValue: function (sValue) {
+			if (!sValue) {
+				return "";
 			}
-		};
 
-	}
-);
+			return parseFloat(sValue).toFixed(2);
+		},
+
+		DisContinuedStatus: function (iNo) {
+			if (iNo < 50) {
+				if (iNo < 20) {
+				return sap.ui.core.ValueState.Error;
+				} else {
+					return sap.ui.core.ValueState.Warning;	
+				}
+			} else {
+				return sap.ui.core.ValueState.Success;
+			}
+		}
+	};
+
+});
